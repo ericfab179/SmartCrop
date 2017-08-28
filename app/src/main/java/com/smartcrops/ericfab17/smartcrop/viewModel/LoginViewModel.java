9446bd33d6635240e55737ea9b7f4f85cc1af088
ericfab17 <ericfab17@gmail.com>
2017-08-28 09:17:49 -0500

    Last update

--- app/src/main/java/com/smartcrops/ericfab17/smartcrop/viewModel/LoginViewModel.java
+++ app/src/main/java/com/smartcrops/ericfab17/smartcrop/viewModel/LoginViewModel.java
@@ -2,16 +2,9 @@ package com.smartcrops.ericfab17.smartcrop.viewModel;
 
 import android.content.Context;
 import android.databinding.BaseObservable;
-import android.support.annotation.NonNull;
 import android.view.View;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.smartcrops.ericfab17.smartcrop.view.MainActivity;
-
-import java.util.concurrent.Executor;
 
 /**
  * Created by ericfab17 on 26/08/17.
@@ -28,7 +21,7 @@ public class LoginViewModel extends BaseObservable {
         Context context = view.getContext();
         //Toast.makeText( context, email+password,Toast.LENGTH_SHORT).show();
 
-        firebaseAuth.getInstance();
+/*        firebaseAuth.getInstance();
         firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -36,7 +29,7 @@ public class LoginViewModel extends BaseObservable {
                     context.startActivity(MainActivity.);
                 }
             }
-        });
+        });*/
 
 
     }
